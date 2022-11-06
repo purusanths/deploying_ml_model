777f815db107c7e512dff1cae7b50493851afdb1
purusanths <purusanth200@gmail.com>
2022-11-06 20:33:51 +0530

    app updated

--- starter/main.py
+++ starter/main.py
@@ -6,35 +6,40 @@ from starter.ml.data import process_data
 import pickle
 import pandas as pd
 
-# model = pickle.load(open('./model/rfc_model.pkl', 'rb'))
-# lb = pickle.load(open('./model/lb', 'rb'))
-# encoder = pickle.load(open('./model/encoder', 'rb'))
-# cat_features = ["workclass","education","marital-status","occupation","relationship","race","sex","native-country"]
-
-class Taggeditem(BaseModel):
-    age: float =Field(example=45)
-    workclass: str =Field(example="State-gov")
-    fnlgt: float =Field(example=77516)
-    education: str =Field(example="Bachelors")
-    education_num: float = Field(example=13,alias="education-num")
-    marital_status: str = Field(example="Never-married",alias="marital-status")
-    occupation: str = Field(example="Adm-clerical")
-    relationship: str = Field(example="Not-in-family")
-    race: str =Field(example="White")
-    sex: str =Field(example="Male")
-    capital_gain: float = Field(example=2174,alias='capital-gain')
-    capital_loss: float =Field(example=0,alias='capital-loss')
-    hours_per_week: float =Field(example=40,alias='hours-per-week')
-    native_country: str =Field(example="United-States",alias='native-country')
-
-
-
+from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/") ## define an endpoint
-async def greeting():
-    return {"Greetings": "Welcome to the MLops World!"}
+@app.get("/")
+def home():
+    return {"message":"Welcoem to the MLops worlds"}
+
+
+
+# class Taggeditem(BaseModel):
+#     age: float =Field(example=45)
+#     workclass: str =Field(example="State-gov")
+#     fnlgt: float =Field(example=77516)
+#     education: str =Field(example="Bachelors")
+#     education_num: float = Field(example=13,alias="education-num")
+#     marital_status: str = Field(example="Never-married",alias="marital-status")
+#     occupation: str = Field(example="Adm-clerical")
+#     relationship: str = Field(example="Not-in-family")
+#     race: str =Field(example="White")
+#     sex: str =Field(example="Male")
+#     capital_gain: float = Field(example=2174,alias='capital-gain')
+#     capital_loss: float =Field(example=0,alias='capital-loss')
+#     hours_per_week: float =Field(example=40,alias='hours-per-week')
+#     native_country: str =Field(example="United-States",alias='native-country')
+
+
+
+
+# app = FastAPI()
+
+# @app.get("/") ## define an endpoint
+# async def greeting():
+#     return {"Greetings": "Welcome to the MLops World!"}
 
 
 # @app.post("/inference/")
